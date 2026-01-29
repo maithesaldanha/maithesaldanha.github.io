436952d8f17786c4ae60df999e3b8c604466cc03
Maithê Saldanha <maithesferrao@gmail.com>
2026-01-28 21:09:57 -0300

    feat: implement new portfolio layout and content

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,10 @@ const title = document.querySelector('.glitch');
 setInterval(() => {
   if (Math.random() > 0.9) {
     title.style.opacity = '0.6';
-    setTimeout(() => {
-      title.style.opacity = '1';
-    }, 80);
+    setTimeout(() => title.style.opacity = '1', 80);
   }
 }, 120);
 
 setInterval(() => {
-  document.body.style.opacity =
-    Math.random() > 0.98 ? '0.97' : '1';
+  document.body.style.opacity = Math.random() > 0.98 ? '0.97' : '1';
 }, 200);
