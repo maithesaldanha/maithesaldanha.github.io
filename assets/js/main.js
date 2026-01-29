d72e96464368a5c70092fb47e7e107372ba00f60
Maithê Saldanha <maithesferrao@gmail.com>
2026-01-28 21:24:36 -0300

    revert changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,5 @@
-const title = document.querySelector('.glitch');
+const glitch = document.querySelector('.glitch');
 
 setInterval(() => {
-  if (Math.random() > 0.9) {
-    title.style.opacity = '0.6';
-    setTimeout(() => {
-      title.style.opacity = '1';
-    }, 80);
-  }
-}, 120);
-
-setInterval(() => {
-  document.body.style.opacity =
-    Math.random() > 0.98 ? '0.97' : '1';
-}, 200);
+  glitch.style.opacity = Math.random() > 0.8 ? '0.7' : '1';
+}, 150);
